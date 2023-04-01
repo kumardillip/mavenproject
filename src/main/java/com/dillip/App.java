d1cdb25e8f22cc6e527dfcabc3ea2cc09d93559f
dillip <kumardillipb9@gmail.com>
2023-04-01 12:58:38 +0530

    5th file

---
+++ src/main/java/com/dillip/App.java
@@ -0,0 +1,13 @@
+package com.dillip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
